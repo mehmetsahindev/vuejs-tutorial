9612f9182ea7fa99bb4f04bf846cef3324e6a328
Mehmet Şahin <msahin8097@gmail.com>
2023-08-08 18:24:54 +0300

    create project, props, slots, events, components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <button @click="toggleModal">Sign Up</button>
+  <teleport to="#modals" v-if="showModal">
+    <Modal title="Sign Up" text="Sign Up and start your journey." @close-modal="toggleModal">
+    <form action="">
+      <div class="input-group">
+        <label>Name*</label>
+        <input type="text" name="name">
+      </div>
+      <div class="input-group">
+        <label>Email*</label>
+        <input type="text" name="email">
+      </div>
+    </form>
+
+    <template v-slot:actions>
+      <a href="#">Sing Up</a>
+      <a href="#">More Info</a>
+    </template>
+  </Modal>
+  </teleport>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: "Hello World!",
+      showModal: false
+    }
+  },
+  components: { Modal },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -23,4 +51,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+<style scoped>
+  button {
+    background-color: crimson;
+    color: #ffffff;
+    padding: 5px 10px;
+    border-radius: 15px;
+    border: none;
+  }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+        <div class="modal-content">
+            <slot></slot>
+        </div>
+        <div class="actions">
+            <slot name="actions"></slot>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["title", "text"],
+    methods: {
+        closeModal() {
+            this.$emit("close-modal")
+        }
+    }
+}
+</script>
+
+<style>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgb(0, 0, 0, 0.5);
+    }
+
+    .modal {
+        width: 400px;
+        margin: 100px auto;
+        background-color: #ffffff;
+        border-radius: 15px;
+        padding: 20px;
+    }
+    .modal .modal-content {
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+    .modal .input-group {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-bottom: 15px;
+    }
+    .modal .input-group input {
+        border: 1px solid #ccc;
+        padding: 10px 5px;
+        display: block;
+        width: 100%;
+        box-sizing: border-box;
+        border-radius: 8px;
+    }
+
+    .modal .actions {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20px;
+    }
+    .modal .actions a {
+        display: block;
+        margin: 0 10px;
+        border: 1px solid #ccc;
+        text-decoration: none;
+        color: #000;
+        padding: 5px 10px;
+        border-radius: 8px;
+    }
+</style>
